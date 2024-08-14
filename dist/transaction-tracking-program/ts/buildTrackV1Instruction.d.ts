1c742bd3fe12f46a9cd2a0ddfae75750185ccdee
Lars Frölich <lars@li.finance>
2024-08-14 16:06:39 +0200

    added /dist with compiled js files

---
+++ dist/transaction-tracking-program/ts/buildTrackV1Instruction.d.ts
@@ -0,0 +1,2 @@
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
+export declare function buildTrackV1Instruction(programId: PublicKey, transaction_id: Uint8Array): Promise<TransactionInstruction>;
