b2051832180b459538c680bbcc9e1206377a0abf
Lars Frölich <lars@li.finance>
2024-09-09 11:54:11 +0200

    added decodeTrackingInstructionFromBytes function to module exports

--- dist/_esm/transaction-tracking-program/index.js
+++ dist/_esm/transaction-tracking-program/index.js
@@ -1,3 +1,4 @@
 export { buildTrackV1Instruction } from "./ts/buildTrackV1Instruction";
 export { deriveTrackingV1PdaAddress } from "./ts/deriveTrackingV1PdaAddress";
 export { decodeTrackingInstruction } from "./ts/decodeTrackingInstruction";
+export { decodeTrackingInstructionFromBytes } from "./ts/decodeTrackingInstruction";
